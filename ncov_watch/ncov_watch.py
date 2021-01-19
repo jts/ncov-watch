2e3f7a1f5e71785f80d719c7a1333ad43a9c9384
Jared Simpson <jared.simpson@gmail.com>
2021-01-19 14:35:30 -0500

    setup basic package, with help from @usajusaj and @fmaguire

---
+++ ncov_watch/ncov_watch.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import argparse
+import pysam
+import sys
+import csv
+import os
+from pathlib import Path
+
+class Variant:
+    def __init__(self, contig, position, reference, alt):
+        self.contig = contig
+        self.position = position
+        self.reference = reference
+        self.alt = alt
+        self.name = None
+
+    def key(self):
+        return ",".join([str(x) for x in [self.contig, self.position, self.reference, self.alt]])
+
+def load_vcf(filename):
+    variants = list()
+    f = pysam.VariantFile(filename,'r')
+    for record in f:
+        if len(record.alts) > 1:
+            sys.stderr.write("Multi-allelic VCF not supported\n")
+            sys.exit(1)
+
+        
+        v = Variant(record.chrom, record.pos, record.ref, record.alts[0])
+        if "Name" in record.info:
+            v.name = record.info["Name"]
+        variants.append(v)
+        
+    return variants
+
+def load_ivar_variants(filename):
+    variants = list()
+    try:
+        with open(filename, 'r') as ifh:
+            reader = csv.DictReader(ifh, delimiter='\t')
+            for record in reader:
+                ref = record["REF"]
+                alt = record["ALT"]
+                if alt[0] == "-":
+                    ref += alt[1:]
+                    alt = ref[0]
+                elif alt[0] == "+":
+                    alt = ref + alt[1:]
+
+                variants.append(Variant(record["REGION"], record["POS"], ref, alt))
+    except:
+        pass
+    return variants
+
+def get_from_directory():
+    find_files = lambda pattern : [ path for path in Path(args.directory).rglob(pattern) ]
+    files = find_files("*pass.vcf") + find_files("*pass.vcf.gz") + find_files("*variants.tsv")
+    for f in files:
+        yield str(f)
+
+def get_from_stdin():
+    for line in sys.stdin:
+        yield line.rstrip()
+
+def main():
+
+    description = 'Report samples containing a variant in the watchlist'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-w', '--watchlist', help='file containing the variants to screen for')
+    parser.add_argument('-d', '--directory', help='root of directories holding variant files')
+    args = parser.parse_args()
+
+    watch_variants = load_vcf(args.watchlist)
+    watch_dict = dict()
+    for v in watch_variants:
+        watch_dict[v.key()] = v.name
+
+    gen_func = get_from_stdin
+    if args.directory:
+        gen_func = get_from_directory
+
+    print("\t".join(["sample", "mutation", "contig", "position", "reference", "alt"]))
+    for f in gen_func():
+        if f.find("variants.tsv") >= 0:
+            variants = load_ivar_variants(f)
+        else:
+            variants = load_vcf(f)
+        for v in variants:
+            if v.key() in watch_dict:
+                name = watch_dict[v.key()]
+                if name is None:
+                    name = "not annotated"
+                print("\t".join([os.path.basename(f), name, v.contig, str(v.position), v.reference, v.alt]))
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+requirements = ['pysam']
+
+setup(
+    name="ncov-watch",
+    packages=["ncov_watch"],
+    version='0.0.1',
+    description='Tools for detecting mutations of interest in SARS-CoV-2 sequencing results',
+    long_description="""Tools for detecting mutations of interest in SARS-CoV-2 sequencing results""",
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    author='Jared Simpson',
+    author_email='jared.simpson@gmail.com',
+    zip_safe=False,
+    package_data={'ncov_watch': ['watchlists/*']},
+    include_package_data=True,
+    url='https://github.com/jts/ncov-watch',
+    download_url='https://github.com/jts/ncov-watch/archive/master.zip',
+
+    classifiers=[
+        'Development Status :: 3 - Beta',
+
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',  # pathlib is born
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'ncov-watch=ncov_watch.ncov_watch:main'  # syntax::  executable=package.module:function
+        ]
+    }
+)
